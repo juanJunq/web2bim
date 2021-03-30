024fb9d3104a95c434ee52482efa1ecb230f0192
Juan Junqueira <juan.marquesjunq@gmail.com>
2021-03-30 10:02:28 -0300

    30/03

--- config/database.js
+++ config/database.js
@@ -79,7 +79,8 @@ module.exports = {
       port: Env.get('DB_PORT', DATABASE_URL.port),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
+      ssl: {rejectUnauthorized: false}
     //  host: Env.get('DB_HOST', 'localhost'),
     //  port: Env.get('DB_PORT', ''),
     //  user: Env.get('DB_USER', 'root'),
